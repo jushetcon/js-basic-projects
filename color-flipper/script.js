3fc421e8c1bbbfb802d24218c8260d8a04a81fe2
Thuramz <b.e.s.dark@gmail.com>
2023-08-25 20:48:42 +0300

    add hex

--- color-flipper/script.js
+++ color-flipper/script.js
@@ -4,10 +4,11 @@ const color = document.querySelector('.color')
 
 btn.addEventListener('click', function () {
 	const randomNumber = getRandomNumber()
+	console.log(randomNumber)
 	document.body.style.backgroundColor = colors[randomNumber]
 	color.textContent = colors[randomNumber]
 })
 
 function getRandomNumber() {
-	return Math.floor(Math.random() * 4)
+	return Math.floor(Math.random() * colors.length)
 }
